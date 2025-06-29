87cd2f95d9ae908a32f8ed16576de8accb5a6d9a
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-06-29 19:27:27 +0700

    Update sirius.lua

--- sirius.lua
+++ sirius.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/sirius'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Moonlight HUB | Rayfield",
